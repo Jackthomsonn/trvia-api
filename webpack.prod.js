42565651be8b0de0a9fb4aeca3e6f09f0907ced5
jthomson-act <hello@jackthomson.co.uk>
2018-03-07 18:59:02 +0000

    Update

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const common = require('./webpack.common.js')
 const merge = require('webpack-merge')
 
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const AutomateRelease = require('automate-release-webpack-plugin')
+const { AutomateRelease } = require('./node_modules/automate-release-webpack-plugin/build')
 
 module.exports = merge(common, {
   plugins: [
